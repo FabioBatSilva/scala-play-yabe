fc73cee614586664649e03899b9e57d607b8b243
Fabio B. Silva <fabio.bat.silva@gmail.com>
2011-11-17 10:28:00 -0200

    first version

---
+++ app/controllers.scala
@@ -0,0 +1,42 @@
+package controllers
+
+import play._
+import play.mvc._
+import models._
+import play.data.validation._
+
+object Application extends Controller {
+    
+    import views.Application._
+    
+    def index = {
+    	val allPosts = Post.allWithAuthorAndComments
+	    html.index(
+	        front = allPosts.headOption, 
+	        older = allPosts.drop(1)
+	    )
+    }
+    
+    def show(id: Long) = {
+    	Post.byIdWithAuthorAndComments(id).map { post =>
+        	html.show(post, post._1.prevNext)
+	    } getOrElse {
+	        NotFound("No such Post")
+	    }
+	}
+    
+    def postComment(postId:Long) = {
+	    val author = params.get("author")
+	    val content = params.get("content")
+	    Validation.required("author", author)
+	    Validation.required("content", content)
+	    if(Validation.hasErrors) {
+	        show(postId)
+	    } else {
+	        Comment.create(Comment(postId, author, content))
+	        flash += "success" -> ("Thanks for posting " + author)
+	        Action(show(postId))
+	    }
+	}
+    
+}
